f168906104353ddb7cd3db4da0e913869d489965
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2023-09-02 00:12:03 -0500

    landing page and half of login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Messages from "./messages";
+import Image from "next/image";
+import NEXTNegroSloganRosado from "/public/NEXTNegroSloganRosado.png";
 
 export default function Login() {
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+    <div className="flex-1 flex flex-col w-full  sm:max-w-md justify-center gap-2 bg-gradient-to-t from-[#6460FF] to-[#0600FF]">
       <Link
         href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm text-white"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -18,15 +20,38 @@ export default function Login() {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1 text-white"
         >
           <polyline points="15 18 9 12 15 6" />
         </svg>{" "}
-        Back
+        Atrás
       </Link>
 
+      <div className="flex w-full justify-center content-center pt-10">
+        <Image
+          src={NEXTNegroSloganRosado}
+          quality={100}
+          alt="Next logo"
+          className="mt-10"
+        />
+      </div>
+
+      <div
+        style={{
+          backgroundColor: "#6460FF",
+        }}
+        className="text-white p-5 w-11/12 rounded-r-xl space-y-6 self-start"
+      >
+        <h1 className="font-extrabold text-4xl">BIENVENIDO A NEXT</h1>
+
+        <p className="text-sm">
+          Llena este formulario para empezar tu recorrido y participar por
+          increíbles premios.
+        </p>
+      </div>
+
       <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground px-8"
         action="/auth/sign-in"
         method="post"
       >
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
           'background-hover': 'hsl(var(--btn-background-hover))',
         },
       },
+      fontFamily: {
+        sans: ['var(--font-lexed)'],
+      },
     },
   },
   plugins: [],
